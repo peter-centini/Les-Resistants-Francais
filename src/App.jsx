51dcdb9da5078deaa5df75a3e22ed8c186ed0551
peter-centini <84769404+peter-centini@users.noreply.github.com>
2021-08-05 18:44:07 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Access from './pages/Access';
+import Refused from './pages/Refused'
+
+
 
 function App() {
   return (
     <div className="App">
-     <h1>hello world</h1>
-     <h2>hello world</h2>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Access}/>
+          <Route path='/refused' component={Refused} />
+      </Switch>
+    </Router>
     </div>
   );
 }
