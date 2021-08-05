c42ed2868dcde2227a72194df8c683267b7116b2
mathieumussano <mussanomathieu@gmail.com>
2021-08-05 18:45:58 +0200

    0

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Access from './pages/Access';
+import Refused from './pages/Refused'
+
+
 
 function App() {
   return (
     <div className="App">
-     <h1>hello world</h1>
-     
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Access}/>
+          <Route path='/refused' component={Refused} />
+      </Switch>
+    </Router>
     </div>
   );
 }
