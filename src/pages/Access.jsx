afffea96abfacd2985dc1f11b7c4cd95a75faa7b
Peter <peter@MacBook-Air-de-Peter.local>
2021-08-06 09:51:57 +0200

    Access.jsx

--- src/pages/Access.jsx
+++ src/pages/Access.jsx
@@ -9,10 +9,10 @@ import"./Access.css"
 function Access() {
         const mypassword="debarquement"
 	    const [password, setPassword] = useState("")
-	    // const [passwordAgain, setPasswordAgain] = useState("Peter")
+	    // const [passwordAgain, setPasswordAgain] = useState("Peter1el")
         let history = useHistory();
         const validation = () => { 
-            if(password === mypassword){
+            if(password == mypassword){
                 history.push("/");
             }else{
                 history.push("/refused");
@@ -24,36 +24,34 @@ function Access() {
         <div className= "bande-son"> <audio src={music}
          controls></audio>
         </div>
-         <p className= "indice">Le mot de passe correspond au résumé de la bande sonore en un seul mot !!</p>
+         <p className= "indice">Le mot de passe correspond au résumé de la bande sonore en un seul mot !</p>
 
             <form onSubmit={validation}>
+                
                 <div className="mdp">
-                <label className="password">Mot de Pass :</label>
-                <input type="password" onChange={e => setPassword(e.target.value)}/>
+                <label className="password">Mot de passe:</label>
+                <input className="pass" type="password" onChange={e => setPassword(e.target.value)}/>
                 </div>
                 {/* <label>Password Again:</label> */}
                 {/* <input type="password" onChange={e => setPasswordAgain(e.target.value)}/> */}
                 <PasswordChecklist className="checklist"
-                    rules={["minLength","match",]}
+                    rules={["minLength","match"]}
                     minLength={12}
                     value={password}
                     valueAgain={mypassword}
                     messages={{
-                        minLength: "Mauvaise longeur",
-                         match:"ok tout bon",
-                        // specialChar: "il manque un caractere.",
+                        minLength: "Le nombre de lettres est bon !",
+                        match: "Le mot de passe est conforme !",
                         // number: "",
                         // capital: "",
                     }}
                     />
-                       
                     <div className= "submit">
-                    <input type="submit"/>
+                    <input className="sub" type="submit"/>
 
                     </div>
-                 </form>
-            </div>
-             
+                    </form>
+                    </div>
 	);
     
 }
