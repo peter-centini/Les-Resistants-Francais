3413b67bc72f7b50c09487f49cebab8b8be3d6a4
anne-marziale <annemarziale@hotmail.com>
2021-08-06 13:57:46 +0200

    commit css

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -12,7 +12,7 @@ function Home() {
             < Slider />
             <div className="text">
                 <h1 className="titletext">Chers résistants</h1>
-                <div className="paragraphe">
+                <div className="par">
                 <p className="par1">Je dis l’intérêt supérieur de la patrie, car cette guerre n'est pas une guerre franco allemande qu'une bataille puisse décider. Cette guerre est une guerre mondiale. Si les forces de la liberté triomphent finalement de celles de la servitude, quel serait le destin d'une France qui se serait soumise à l'ennemi ?</p>
                 <p className="par2">L'honneur, le bon sens, l'intérêt supérieur de la patrie commandent à tous les Français libres de continuer le combat, là où ils seront et comme ils pourront. Il est par conséquent nécessaire, de grouper partout où cela se peut, une force française aussi grande que possible. Tout ce qui peut être réuni, en fait d'éléments militaires français et de capacité française de production d'armements doit être organisé partout où il y en a. </p>
                 <p className="par3">Moi, général de Gaulle, j'entreprends ici, en Angleterre, cette tâche nationale. J'invite tous les militaires français des armées de terre, de mer et de l'air. J'invite les chefs, les soldats, les marins, les aviateurs des forces françaises de terre, de mer, de l'air, où qu'ils se trouvent actuellement, à se mettre en rapport avec moi. J'invite tous les Français qui veulent rester libres à m'écouter et à me suivre. Vive la France, libre, dans l'honneur et dans l'indépendance.</p>
