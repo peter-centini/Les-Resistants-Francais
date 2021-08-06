9e4158b7002423f51f9c92ff5123bdc226c029c7
Peter <peter@MacBook-Air-de-Peter.local>
2021-08-06 09:48:16 +0200

    refused jsx

--- src/pages/Refused.jsx
+++ src/pages/Refused.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import "./Refused.css"
+import { Link } from 'react-router-dom'
+
 function refuse() {
     return (
-        <p className="non">ENTREE INTERDITE AUX COLLABOS !!</p>
+ 
+        <div>
+         <h1 className="titre"> Accé refuser car tu as rentrer le mauvais mot de passe</h1>
+          <p className="non">ENTREE INTERDITE AUX COLLABOS !!</p>
+            <Link className="backhome" to={{pathname: '/'}}>retour vers Page Acceuil !!!</Link>     
+       </div>
 
-       
-        
     )
 }
 
