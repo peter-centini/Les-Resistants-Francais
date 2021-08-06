b153daff73a6e593d14b440598848fe853bc13ed
Maryline-Bellanger <maryline-bellanger_student2021@wilder.school>
2021-08-06 07:32:07 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import HomeDetails from './components/Home/HomeDetails';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    
+      <Router>
+        <Switch>
+          <Route path='/Detail/:id' component={HomeDetails} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Home/HomeDetails.jsx
+++ src/components/Home/HomeDetails.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
-function HomeDetails() {
+function HomeDetails(props) {
+    const [detail, setDetail] = useState([]);
+    const resistId = props.match.params.id;
+
+    useEffect(() => {
+        axios
+        .get(`https://a.nacapi.com/resistant/${resistId}`)
+        .then((res) => res.data)
+        .then((data) => setDetail(data))
+    }, [resistId])
+
     return (
-        <div>
-            
+        <div className='HeroDetails'>
+            <div className='detail-card'>
+            <img className='image-detail' src={detail.image} alt={detail.description} />
+                <div className='detail-card-content'>
+                    <h2>{detail.name}</h2>
+                    <p>Description : {detail.description}</p>
+                </div>
+            </div>
+            <Link className='link-detail' to={{pathname:'/'}}>Retour à la page principale</Link>
         </div>
     )
 }
