4586e38a76cf121bf8089fbcdb1ca716e5a67685
Maryline-Bellanger <maryline-bellanger_student2021@wilder.school>
2021-08-06 11:26:34 +0200

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import HomeDetails from './components/Home/HomeDetails';
 import Home from './components/Home/Home';
 import './App.css';
-import Access from './pages/Access';
-import Refused from './pages/Refused';
+//import Access from './pages/Access';
+//import Refused from './pages/Refused';
 
 
 
@@ -12,8 +12,6 @@ function App() {
     <div className="App">  
     <Router>
       <Switch>
-        <Route exact path='/' component={Access}/>
-        <Route path='/refused' component={Refused} />
         <Route path='/Home' component={Home} /> 
         <Route path='/Detail/:id' component={HomeDetails} />
       </Switch>
